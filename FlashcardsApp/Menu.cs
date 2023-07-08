3b8139be1c5970ad8aefbcb3cc8cfc7adba20ec1
joshhkim02 <joshhkim02@gmail.com>
2023-07-08 16:27:47 -0700

    Added stack choice validation

--- FlashcardsApp/Menu.cs
+++ FlashcardsApp/Menu.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace FlashcardsApp
 {
-    class Menu
+    internal class Menu
     {
         static string connectString = Path.Combine(AppContext.BaseDirectory, "FlashcardsDB.db");
         static string connectionString = $"Data Source= {connectString}";
@@ -16,41 +16,44 @@ namespace FlashcardsApp
 
         internal void showMenu()
         {
-            Console.Clear();
-            Console.WriteLine("-------------------------------------------------");
-            Console.WriteLine("Welcome to your flashcards!");
-            Console.WriteLine("Enter 1 to create a new stack.");
-            Console.WriteLine("Enter 2 to view existing stacks.");
-            Console.WriteLine("Enter 3 to edit an existing stack.");
-            Console.WriteLine("Enter 4 to exit.");
-            Console.WriteLine("-------------------------------------------------");
+            bool closeApp = false;
+            while (closeApp == false)
+            { 
+                Console.Clear();
+                Console.WriteLine("-------------------------------------------------");
+                Console.WriteLine("Welcome to your flashcards!");
+                Console.WriteLine("Enter 1 to create a new stack.");
+                Console.WriteLine("Enter 2 to view existing stacks.");
+                Console.WriteLine("Enter 3 to edit an existing stack.");
+                Console.WriteLine("Enter 4 to exit.");
+                Console.WriteLine("-------------------------------------------------");
 
-            var userInput = Console.ReadLine();
+                var userInput = Console.ReadLine();
 
-            switch (userInput)
-            {
-                case "1":
-                    createNewStack();
-                    break;
-                case "2":
-                    showStacks();
-                    Console.WriteLine("Enter any key to return to the menu.");
-                    Console.ReadLine();
-                    showMenu();
-                    break;
-                case "3":
-                    showStacks();
-                    editStack();
-                    
-                    break;
-                case "4":
-                    addCard();
-                    break;
-                default:
-                    Console.WriteLine("Invalid input. Enter any key to try again.");
-                    Console.ReadLine();
-                    break;
-
+                switch (userInput)
+                {
+                    case "1":
+                        createNewStack();
+                        break;
+                    case "2":
+                        showStacks();
+                        Console.WriteLine("Enter any key to return to the menu.");
+                        Console.ReadLine();
+                        showMenu();
+                        break;
+                    case "3":
+                        showStacks();
+                        createStack.showCreateMenu();
+                        break;
+                    case "4":
+                        Console.WriteLine("Exiting...");
+                        closeApp = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input. Enter any key to try again.");
+                        Console.ReadLine();
+                        break;
+                }
             }
         }
 
@@ -133,55 +136,5 @@ namespace FlashcardsApp
             }
         }
 
-        internal void editStack()
-        {
-            Console.WriteLine("Enter the ID of the stack you would like to edit.");
-            var userChoice = Console.ReadLine();
-
-            using (var connection = new SqliteConnection(connectionString))
-            {
-                connection.Open();
-                var tableCmd = connection.CreateCommand();
-                tableCmd.CommandText =
-                    $"SELECT StackID from Stack";
-
-                List<Stack> checkStacks = new();
-
-                SqliteDataReader reader = tableCmd.ExecuteReader();
-
-                if (reader.HasRows)
-                {
-                    while (reader.Read())
-                    {
-                        checkStacks.Add(
-                            new Stack
-                            {
-                                StackID = reader.GetInt32(0),
-                            });
-                    }
-                }
-                else Console.WriteLine("No rows found.");
-                connection.Close();
-
-                int.TryParse(userChoice, out var intChoice);
-
-                bool doesExist = false;
-
-                foreach (var stack in checkStacks)
-                {
-                    if (intChoice == stack.StackID)
-                    {
-                        Console.WriteLine("ID entered does exist!");
-                        doesExist = true;
-                    }
-                }    
-                if (doesExist == false)
-                {
-                    Console.WriteLine("ID does not exist, please try again.");
-                    editStack();
-                }    
-                
-            }
-        }
     }
 }
--- FlashcardsApp/createStackMenu.cs
+++ FlashcardsApp/createStackMenu.cs
@@ -9,36 +9,92 @@ namespace FlashcardsApp
 {
     internal class createStackMenu
     {
+        static string connectString = Path.Combine(AppContext.BaseDirectory, "FlashcardsDB.db");
+        static string connectionString = $"Data Source= {connectString}";
         internal void showCreateMenu()
         {
-            Console.Clear();
-
-            Console.WriteLine("-------------------------------------------------");
-            Console.WriteLine("Enter 1 to create a new card.");
-            Console.WriteLine("Enter 2 to view existing cards.");
-            Console.WriteLine("Enter 3 to delete an existing card.");
-            Console.WriteLine("Enter 4 when you are finished.");
-            Console.WriteLine("-------------------------------------------------");
-
-            var userInput = Console.ReadLine();
-
-            switch (userInput)
+            bool closeApp = false;
+            while (closeApp == false)
             {
-                case "1":
-                    Console.WriteLine("Creating new card...");
-                    break;
-                case "2":
-                    Console.WriteLine("Viewing existing cards...");
-                    break;
-                case "3":
-                    Console.WriteLine("Deleting an existing card...");
-                    break;
-                case "4":
-                    Console.WriteLine("Finishing...");
-                    break;
-                default:
-                    Console.WriteLine("Invalid input. Enter any key to try again.");
-                    break;
+                validateChoice();
+                Console.WriteLine("-------------------------------------------------");
+                Console.WriteLine("Enter 1 to create a new card.");
+                Console.WriteLine("Enter 2 to view existing cards.");
+                Console.WriteLine("Enter 3 to delete an existing card.");
+                Console.WriteLine("Enter 4 when you are finished.");
+                Console.WriteLine("-------------------------------------------------");
+
+                var userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        Console.WriteLine("Creating new card...");
+                        break;
+                    case "2":
+                        Console.WriteLine("Viewing existing cards...");
+                        break;
+                    case "3":
+                        Console.WriteLine("Deleting an existing card...");
+                        break;
+                    case "4":
+                        Console.WriteLine("Finishing...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input. Enter any key to try again.");
+                        break;
+                }
+            }
+        }
+        internal void validateChoice()
+        {
+            Console.WriteLine("Enter the ID of the stack you would like to edit.");
+            var userChoice = Console.ReadLine();
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+                tableCmd.CommandText =
+                    $"SELECT StackID from Stack";
+
+                List<Stack> checkStacks = new();
+
+                SqliteDataReader reader = tableCmd.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        checkStacks.Add(
+                            new Stack
+                            {
+                                StackID = reader.GetInt32(0),
+                            });
+                    }
+                }
+                else Console.WriteLine("No rows found.");
+                connection.Close();
+
+                int.TryParse(userChoice, out var intChoice);
+
+                bool isFound = false;
+
+                foreach (var stack in checkStacks)
+                {
+                    if (intChoice == stack.StackID)
+                    {
+                        isFound = true;
+                    }
+                }
+                if (isFound == true)
+                {
+                    Console.WriteLine("The ID entered exists.");
+                }
+                else if (isFound == false)
+                {
+                    Console.WriteLine("The ID entered does not exist.");
+                }
             }
         }
     }
