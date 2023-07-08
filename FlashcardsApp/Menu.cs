655a5833f0a71f329268be917f868d5ddbb3c7a0
joshhkim02 <joshhkim02@gmail.com>
2023-07-08 15:24:48 -0700

    Created functionality to add a card to the database

--- FlashcardsApp/Menu.cs
+++ FlashcardsApp/Menu.cs
@@ -16,11 +16,13 @@ namespace FlashcardsApp
 
         internal void showMenu()
         {
+            Console.Clear();
             Console.WriteLine("-------------------------------------------------");
             Console.WriteLine("Welcome to your flashcards!");
             Console.WriteLine("Enter 1 to create a new stack.");
             Console.WriteLine("Enter 2 to view existing stacks.");
-            Console.WriteLine("Enter 3 to exit.");
+            Console.WriteLine("Enter 3 to edit an existing stack.");
+            Console.WriteLine("Enter 4 to exit.");
             Console.WriteLine("-------------------------------------------------");
 
             var userInput = Console.ReadLine();
@@ -29,14 +31,20 @@ namespace FlashcardsApp
             {
                 case "1":
                     createNewStack();
-                    //createStack.showCreateMenu();
                     break;
                 case "2":
-                    Console.WriteLine("Viewing existing stacks...");
-                    testDatabase();
+                    showStacks();
+                    Console.WriteLine("Enter any key to return to the menu.");
+                    Console.ReadLine();
+                    showMenu();
                     break;
                 case "3":
-                    Console.WriteLine("Exiting...");
+                    showStacks();
+                    editStack();
+                    
+                    break;
+                case "4":
+                    addCard();
                     break;
                 default:
                     Console.WriteLine("Invalid input. Enter any key to try again.");
@@ -46,8 +54,32 @@ namespace FlashcardsApp
             }
         }
 
+        internal void addCard()
+        {
+            Console.WriteLine("Enter in the ID of the stack you want to add this to.");
+            var stackID = Console.ReadLine(); 
+
+            Console.WriteLine("What would you like to write on the front?");
+            var frontDesc = Console.ReadLine();
+
+            Console.WriteLine("What would you like to write on the back?");
+            var backDesc = Console.ReadLine();
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+                tableCmd.CommandText =
+                    $"INSERT INTO Flashcard(StackID, FrontDesc, BackDesc) VALUES ('{stackID}', '{frontDesc}', '{backDesc}')";
+
+                tableCmd.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
         internal void createNewStack()
         {
+            Console.Clear();
             Console.WriteLine("Enter in the name of your new stack: ");
             var stackName = Console.ReadLine();
 
@@ -63,16 +95,17 @@ namespace FlashcardsApp
             }
         }
 
-        internal void testDatabase()
+        internal void showStacks()
         {
+            Console.Clear();
             using (var connection = new SqliteConnection(connectionString))
             {
                 connection.Open();
                 var tableCmd = connection.CreateCommand();
                 tableCmd.CommandText =
-                    $"SELECT * FROM Flashcard";
+                    "SELECT * FROM Stack";
 
-                List<Flashcard> personData = new();
+                List<Stack> stacks = new();
 
                 SqliteDataReader reader = tableCmd.ExecuteReader();
 
@@ -80,25 +113,74 @@ namespace FlashcardsApp
                 {
                     while (reader.Read())
                     {
-                        personData.Add(
-                            new Flashcard
+                        stacks.Add(
+                            new Stack
                             {
-                                CardID = reader.GetInt32(0),
-                                StackID = reader.GetInt32(1),
-                                FrontDesc = reader.GetString(2),
-                                BackDesc = reader.GetString(3)
+                                StackID = reader.GetInt32(0),
+                                StackName = reader.GetString(1)
                             });
                     }
-                }
-                else Console.WriteLine("No rows found");
+                } else Console.WriteLine("No rows found.");
                 connection.Close();
 
                 Console.WriteLine("-----------------------------------");
-                foreach (Flashcard card in personData)
+                foreach (var stack in stacks)
                 {
-                    Console.WriteLine($"{card.CardID} - {card.StackID} - {card.FrontDesc} - {card.BackDesc}");
+                    Console.WriteLine($"StackID: {stack.StackID}");
+                    Console.WriteLine($"Stack name: {stack.StackName}\n");
                 }
                 Console.WriteLine("-----------------------------------");
+            }
+        }
+
+        internal void editStack()
+        {
+            Console.WriteLine("Enter the ID of the stack you would like to edit.");
+            var userChoice = Console.ReadLine();
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+                tableCmd.CommandText =
+                    $"SELECT StackID from Stack";
+
+                List<Stack> checkStacks = new();
+
+                SqliteDataReader reader = tableCmd.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        checkStacks.Add(
+                            new Stack
+                            {
+                                StackID = reader.GetInt32(0),
+                            });
+                    }
+                }
+                else Console.WriteLine("No rows found.");
+                connection.Close();
+
+                int.TryParse(userChoice, out var intChoice);
+
+                bool doesExist = false;
+
+                foreach (var stack in checkStacks)
+                {
+                    if (intChoice == stack.StackID)
+                    {
+                        Console.WriteLine("ID entered does exist!");
+                        doesExist = true;
+                    }
+                }    
+                if (doesExist == false)
+                {
+                    Console.WriteLine("ID does not exist, please try again.");
+                    editStack();
+                }    
+                
             }
         }
     }
